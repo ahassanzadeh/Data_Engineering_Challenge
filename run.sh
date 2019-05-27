a07604219da1d957e3a2ee3fda66e8bc43b4767b
ali <ahassanzadeh@ucmerced.edu>
2019-05-27 13:29:22 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
+# use Python3
 work_path=$(dirname $0)
 
 cd ${work_path}/src/
